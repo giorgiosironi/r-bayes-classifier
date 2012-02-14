644d23e9e86c602a5da04f23815e4d8fd2ba358b
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2012-02-14 09:57:30 +0100

    Introduced features and their probabilities estimation

--- example.r
+++ example.r
@@ -1,25 +1,61 @@
+contains <- function(a_list, key) {
+    return (length(which(names(a_list) == key)) > 0)
+}
+
 # training set
-spam <- list(c("buy", "drugs", "online", "from", "our", "pharma"),
-            c("buy", "insurance", "at", "low", "prices"))
-legitimate <- list(c("newsletter", "from", "your", "favorite", "website"),
-                   c("I", "was", "writing", "for", "php", "advice"),
+training <- list()
+training[[1]] <- list(c("buy", "drugs", "online", "from", "our", "pharma"),
+                    c("buy", "insurance", "at", "low", "prices"))
+training[[2]] <- list(c("newsletter", "from", "your", "favorite", "website"),
+                    c("I", "was", "writing", "for", "php", "advice"),
                    c("good", "article", "on", "php"))
-# training
-categories = 2
+categories <- length(training)
+## priors
 priors <- c()
-total <- length(spam) + length(legitimate)
-priors[1] <- length(spam) / total
-priors[2] <- length(legitimate) / total
+total = 0
+for (category in 1:categories) {
+    total <- total + length(training[[category]])
+}
+for (category in 1:categories) {
+    priors[category] <- length(training[[category]]) / total
+}
+## features
+features <- list();
+zeroOccurrences = list()
+for (category in 1:categories) {
+    categoryFeatures <- list();
+    singleOccurrence = 1 / length(training[[category]])
+    zeroOccurrences[[category]] = singleOccurrence
+    for (sampleMail in training[[category]]) {
+        for (word in sampleMail) {
+            if (contains(categoryFeatures, word)) {
+                categoryFeatures[[word]] = categoryFeatures[[word]] + singleOccurrence
+            } else {
+                categoryFeatures[[word]] = zeroOccurrences[[category]] + singleOccurrence
+            }
+        }
+    }
+    features[[category]] <- categoryFeatures
+}
 
 # classifier
 score <- function (test_mail, category) {
-    priors[category]
+    score <- priors[category]
+    categoryFeatures = features[[category]]
+    for (word in test_mail) {
+        if (contains(categoryFeatures, word)) {
+            score <- score * categoryFeatures[[word]]
+        } else {
+            score <- score * zeroOccurrences[[category]]
+        }
+    }
+    return(score)
 }
 
 classify <- function(test_mail) {
-    scores = c()
+    scores <- c()
     for (i in 1:categories) {
-        scores[i] = score(test_mail, i)
+        scores[i] <- score(test_mail, i)
     }
     print(scores)
     which(scores==max(scores))
